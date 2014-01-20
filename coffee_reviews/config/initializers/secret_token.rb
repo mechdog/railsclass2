83ad23c42e3bc75d76af3af7e96c4430a7315fab
Mark Kenney <mark@kenneywww.com>
2014-01-20 17:37:09 -0500

    Chapter 7 start of Rails application

---
+++ coffee_reviews/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeReviews::Application.config.secret_key_base = 'b898c51bccceb92a7d7f99b23f6cab0f1d7b53276506f64a22c3e9d0731abc1fcd5d724f35930272c732e828b47d84937b639985c1c802b54626939db36c2b37'
