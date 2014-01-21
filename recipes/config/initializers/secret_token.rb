cdbd4c6886aa618a85d84d6b1507718a2ad8fc4f
Mark Kenney <mark@kenneywww.com>
2014-01-21 15:29:04 -0500

    Start of Chapter 14 - Routing - Recipe app created

---
+++ recipes/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recipes::Application.config.secret_key_base = '43d40d082d521f246d0ff2cbf180c30865a4dfebe37e88f30cc66868e3077511cb2b8d2df4d608080fd76026f305f1fd41fcf47d47b889acd578d8aaba5f6999'
