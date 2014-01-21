c8d62c0087bd51617b558e240f74b9b63b960784
Mark Kenney <mark@kenneywww.com>
2014-01-21 09:36:57 -0500

    New project for chapter 11 Lost_dog

---
+++ lost_dog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LostDog::Application.config.secret_key_base = '1970ba1322b603b8bbad41c5bbc6552661475bf91324786f0c815b98d6975f21caad074e03fddfef0ab748e01db630bbf7fe2b87352cad4bbd6a515803493f92'
