403b53e50916abd8233f2c135bbb90802554377c
Mark Kenney <mark@kenneywww.com>
2014-01-22 10:43:30 -0500

    Start of Chapter 18- Assets

---
+++ utilities_dashboard/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UtilitiesDashboard::Application.config.secret_key_base = '236dcb558e58e3f33bec1426fe9a232455cfe0d29ff1c619b30d4a26a64adf6b2702b3c30787ffc10aae0b80cb4fb71f00b2a72a67865babb7d67c539a10fef1'
