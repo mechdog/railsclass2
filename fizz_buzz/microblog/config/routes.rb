c2b1cdd742d6fdd537b47356f5bf824f8ab7d122
Mark Kenney <mark@kenneywww.com>
2014-01-23 16:29:17 -0500

    Testing chapter

--- fizz_buzz/microblog/config/routes.rb
+++ fizz_buzz/microblog/config/routes.rb
@@ -2,6 +2,8 @@ Microblog::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'posts#index'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
---
+++ fizz_buzz/microblog/spec/features/viewing_posts.feature
@@ -0,0 +1,8 @@
+Feature: Viewing posts
+  Background:
+    Given a post exists
+
+  Scenario: view posts
+    When I visit the root page
+    Then I should see a post
+    
