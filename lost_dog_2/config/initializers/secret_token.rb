2a2791a785b4e17449041bd006d99dc1b1893331
Mark Kenney <mark@kenneywww.com>
2014-01-23 09:51:30 -0500

    Uploading images chapter start

---
+++ lost_dog_2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LostDog2::Application.config.secret_key_base = '625870e488266c9682c3ce56e36566c0020a76dc8393e0e60f333a26ca0bc30358b6ef99f868d8f25cef898b30091c6430ac2d65c5e9b80af2d94c0d7372e479'
---
+++ lost_dog_2/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LostDog2::Application.config.session_store :cookie_store, key: '_lost_dog_2_session'
