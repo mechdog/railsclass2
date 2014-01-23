f6e22dbfb47a9e7605b541a78fd070cec75b1657
Mark Kenney <mark@kenneywww.com>
2014-01-23 14:12:58 -0500

    Nerd News beginning

---
+++ nerdnews/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nerdnews::Application.config.secret_key_base = '814f62fc219183d502a4a8138356be710bd7fc8eaf94360486c6d21ef3b23c4e4c01c5c2912749130623581fdd3b98a7bc26db5b1a18c03e4871bd5ebe33a982'
