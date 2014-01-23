1948268ceb6eed65e63fbe6747c205d38cfe7389
Mark Kenney <mark@kenneywww.com>
2014-01-23 14:44:52 -0500

    Rspec testing

--- fizz_buzz/fizz_buzz_spec.rb
+++ fizz_buzz/fizz_buzz_spec.rb
@@ -1,5 +1,36 @@
 class FizzBuzz
+  def self.at(n)
+    return "FizzBuzz" if n % 3 == 0 && n % 5 == 0
+    return "Fizz" if n % 3 == 0
+    return "Buzz" if n % 5 == 0
+    n
+  end
 end
 
 describe FizzBuzz do
+  it 'returns 1 for 1' do
+    expect(FizzBuzz.at(1)).to eq(1)
+  end
+
+  it 'returns Fizz for 3' do
+    expect(FizzBuzz.at(3)).to eq("Fizz")
+  end
+
+  it 'returns Buzz for 5' do
+    expect(FizzBuzz.at(5)).to eq("Buzz")
+  end
+
+  it 'returns Fizz for 6' do
+    expect(FizzBuzz.at(6)).to eq("Fizz")
+  end
+
+  it 'returns Buzz for 15' do
+    expect(FizzBuzz.at(15)).to eq("FizzBuzz")
+  end
+
+  it 'returns 2 for 2' do
+    expect(FizzBuzz.at(2)).to eq(2)
+  end
+
+
 end
